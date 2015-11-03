9565a20a9fcc5daa259d3a29ad554703d7d30d08
tarteka <info@tarteka.net>
2015-11-03 16:57:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+Easy Archive.org Embed for Jekyll
+==============
+
+A plugin for Jekyll to easily embed [Archive.org](https://arvhive.org) videos, audios and audio lists into your site.
+
+> Based on [Easy Youtube Embed for Jekyll](https://github.com/pibby/jekyll-youtube) by [Katie Harron](https://pibby.com/)
+
+## Demo
+[Jekyll Archive.org Plugin](https://tarteka/2015/11/insertar-audios-y-vdeos-de-archiveorg-en-jekyll/)
+
+## Installation Instructions
+Download the file `archive.rb` and place it in your `_plugins` folder of your Jekyll installation.
+
+## Usage Instructions
+In your Markdown post, simply include the following command:
+
++ `{% archive_video VIDEO_ID %}`
++ `{% archive_audio AUDIO_ID %}`
++ `{% archive_list AUDIO_LIST_ID %}`
+
+Video example, `{% archive_video TimeLapseUsandoMEncoder %}` will render as:  
+```html
+<div class="video">
+  <figure>
+    <iframe width="800" height="480" src="//archive.org/embed/TimeLapseUsandoMEncoder" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+  </figure>
+</div>
+```
+
+Audio example, `{% archive_audio killallradiojingle %}` will render as:
+
+```html
+<div class="audio">
+  <iframe width="800" height="30" src="//archive.org/embed/killallradiojingle" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+</div>
+```
+
+Audio List example, `{% archive_list rsnlrckmtrkrsnlrckmtrk %}` will render as:
+
+```html
+<div class="video">
+  <figure>
+    <iframe width="800" height="300" src="//archive.org/embed/rsnlrckmtrkrsnlrckmtrk" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+  </figure>
+</div>
+```
+
+---
+
+If you're interested in the CSS, you can add it to your stylesheet and your videos will resize responsively for users on mobile devices.
+
+```css
+/* responsive videos */
+.video figure {
+	position: relative;
+	nowhitespace: afterproperty;
+	padding-bottom: 56.25%;
+	height: 0;
+	overflow: hidden;
+}
+
+.video iframe, .video object, .video embed {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+.audio iframe, .audio object, .audio embed {
+  width: 100%;
+}
+```
---
+++ archive.rb
@@ -0,0 +1,108 @@
+# Jekyll - Easy Archive.org Embed
+#
+# Una forma sencilla de insertar vídeos, audios y listas de audios de Archive.org
+# Un fork basado en "Easy Youtube Embed" de Katie Harron - https://github.com/pibby
+#
+#   Uso:
+#     Insertar un vídeo: {% archive_video VIDEO_ID %}
+#     Insertar un audio: {% archive_audio AUDIO_ID %}
+#     Insertar una lista de audio: {% archive_list AUDIO_LIST_ID %}
+#
+########################
+# CSS responsive
+# código a añadir en archivo css
+#
+# /* responsive videos */
+# .video figure {
+#    position: relative;
+#    nowhitespace: afterproperty;
+#    padding-bottom: 56.25%;
+#    height: 0;
+#    overflow: hidden;
+# }
+#
+# .video iframe, .video object, .video embed {
+#    position: absolute;
+#    top: 0;
+#    left: 0;
+#    width: 100%;
+#    height: 100%;
+# }
+#
+# .audio iframe, .audio object, .audio embed {
+#    width: 100%;
+# }
+#
+
+module Jekyll
+  class Archive_video < Liquid::Tag
+    @url = nil
+
+    VIDEO_URL = /(\S+)/i
+
+    def initialize(tag_name, markup, tokens)
+      super
+
+      if markup =~ VIDEO_URL
+        @url = $1
+      end
+    end
+
+    def render(context)
+      source = "<div class=\"video\">"
+      source += "<figure>"
+      source += "<iframe width=\"800\" height=\"480\" src=\"//archive.org/embed/#{@url}\" frameborder=\"0\" allowfullscreen webkitallowfullscreen=\"true\" mozallowfullscreen=\"true\"></iframe>"
+      source += "</figure>"
+      source += "</div>"
+      source
+    end
+  end
+
+  class Archive_audio < Liquid::Tag
+    @url = nil
+
+    AUDIO_URL = /(\S+)/i
+
+    def initialize(tag_name, markup, tokens)
+      super
+
+      if markup =~ AUDIO_URL
+        @url = $1
+      end
+    end
+
+    def render(context)
+      source = "<div class=\"audio\">"
+      source += "<iframe width=\"800\" height=\"30\" src=\"//archive.org/embed/#{@url}\" frameborder=\"0\" allowfullscreen webkitallowfullscreen=\"true\" mozallowfullscreen=\"true\"></iframe>"
+      source += "</div>"
+      source
+    end
+  end
+
+  class Archive_list < Liquid::Tag
+    @url = nil
+
+    LIST_URL = /(\S+)/i
+
+    def initialize(tag_name, markup, tokens)
+      super
+
+      if markup =~ LIST_URL
+        @url = $1
+      end
+    end
+
+    def render(context)
+      source = "<div class=\"video\">"
+      source += "<figure>"
+      source += "<iframe width=\"800\" height=\"300\" src=\"//archive.org/embed/#{@url}&playlist=1\" frameborder=\"0\" allowfullscreen webkitallowfullscreen=\"true\" mozallowfullscreen=\"true\"></iframe>"
+      source += "</figure>"
+      source += "</div>"
+      source
+    end
+  end
+end
+
+Liquid::Template.register_tag('archive_video', Jekyll::Archive_video)
+Liquid::Template.register_tag('archive_audio', Jekyll::Archive_audio)
+Liquid::Template.register_tag('archive_list', Jekyll::Archive_list)
