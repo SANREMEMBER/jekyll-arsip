9a10bdb8f5ba93e71f9a833e09245c021dc06267
SANREMEMBER <sanremember@gmx.com>
2016-11-02 08:54:19 +0700

    good for startting

--- archive.rb
+++ archive.rb
@@ -105,25 +105,50 @@ module Jekyll
 
 
   class Archive_image < Liquid::Tag
-    @url = nil
-    @page_title = ![{{page.title}}]
 
-    IMAGE_URL = /(\S+)/i
-
-    def initialize(tag_name, markup, tokens)
-      super
-
-      if markup =~ IMAGE_URL
-        @url = $1
-      end
-    end
-    
-    def render(context)
-      source = "#{@page_title}(https://archive.org/download/#{@url})"
-      source
-    end
+  def initialize(tag_name, input, tokens)
+    super
+    @input = input
   end
 
+  # Lookup allows access to the page/post variables through the tag context
+  # def lookup(context, name)
+  #  lookup = context
+  #  name.split(".").each { |value| lookup = lookup[value] }
+  #  lookup
+  # end
+
+  def render(context)
+
+    # Accessing the page/site variable for the base url
+    archive = "https://archive.org/download"
+
+    # Reading the tag parameter (using the pipe-split technique)
+    input_split = split_params(@input)    
+    img_path = input_split[0].strip.downcase
+    # Caption is an optional second parameter
+    if( input_split.length > 1 )
+      caption = input_split[1].strip
+    end
+
+    # Create the HTML output for the image container with an optional caption
+    # the 'captioned-image' css class controls the look and feel of the image
+    # in my case the class centeres the image and poses maximum size restrictions
+    output =  "<div class=\"image\"><div>"
+    output += "<img src=\"#{archive}/#{img_path}\" alt=\"#{caption}\" title=\"#{caption}\">"
+    if( !caption.nil? && !caption.empty? )
+      output += "<p>#{caption}</p>"
+    end
+    output += "</div></div>"
+
+    return output    
+  end
+
+  def split_params(params)
+    params.split("|")
+  end
+  end
+  
 end
 
 Liquid::Template.register_tag('archive_video', Jekyll::Archive_video)
